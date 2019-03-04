b7c2a10a5c26dacbcda1b572460516fb5c26078e
Joshua Bair <josh.bair@jenkon.com>
2019-03-04 09:57:43 -0600

    Added separate projects for C64 cartridge vs PRG files.

--- C64Console/BuildCartridge.cs
+++ C64Console/BuildCartridge.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 using OZone.Programs;
 using OZone.Programs.Compilers;
+using OZone.Projects;
 
 namespace C64Console
 {
@@ -12,53 +14,92 @@ namespace C64Console
 	{
 		static void Main(string[] args)
 		{
-			var compiler = new BinaryCompiler();
+			var project = ProjectReader.Read("../../Projects/ProjectCartridge.xml");
 
-			var program = ProgramBuilder.Build("../../C64Cartridge.xml");
-			//var program = ProgramBuilder.Build("../../C64Program.xml");
+			//ProjectBuilder.Clean(project);
 
-			var address = new MemoryAddress { Offset = 0x8000 };
-			//var address = new MemoryAddress { Offset = 0x07ff };
+			ProjectBuilder.Build(project);
 
-			using(Stream stream = File.Create("C64Console.bin"))
-			//using (Stream stream = File.Create("C64Console.prg"))
-			using (BinaryWriter binaryWriter = new BinaryWriter(stream))
+			var image = new CartridgeImage();
+
+			foreach (var file in project.Files)
 			{
-				compiler.Compile(program, address, binaryWriter);
+				var filename = file.OutputPath;
 
-				program = ProgramBuilder.Build(
-					"../../C64Console.xml",
-					new KeyValuePair<string, string>[]
-					{
-						new KeyValuePair<string, string>("http://metalx.org/C64Console/Functions/Screen", "../../Functions/Screen.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/C64Console/Functions/System", "../../Functions/System.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/6502/Functions/Class", "../../../OZone/Functions/6502/Class.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/6502/Functions/String", "../../../OZone/Functions/6502/String.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/6502/Functions/Memory", "../../../OZone/Functions/6502/Memory.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/6502/Functions/MemoryManager", "../../../OZone/Functions/6502/MemoryManager.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Commodore64/Kernel", "../../../OZone/Platforms/Commodore/C64/Kernel.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Commodore64/Keyboard", "../../../OZone/Platforms/Commodore/C64/Keyboard.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Commodore64/System", "../../../OZone/Platforms/Commodore/C64/System.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Commodore64/Video", "../../../OZone/Platforms/Commodore/C64/Video.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Commodore64/Petscii", "../../../OZone/Platforms/Commodore/C64/Petscii.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Commodore64/Color", "../../../OZone/Platforms/Commodore/C64/Color.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Ansi/Ascii", "../../../OZone/Platforms/Ansi/Ascii/Ascii.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Variable", "../../../OZone/Structures/6502/Variable.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Class", "../../../OZone/Structures/6502/Class.xslt"),
-						new KeyValuePair<string, string>("http://metalx.org/Mos/6502/Operators", "../../../OZone/Platforms/Mos/6502/Operators.xslt")
-					});
+				try
+				{
+					var document = XDocument.Load(filename, LoadOptions.SetLineInfo);
 
-				//address.Offset = 0x8000;
-				//address.Offset = 0x810;
-				address.Offset = (uint)binaryWriter.BaseStream.Position + 0x8000;
-				//address.Offset = (uint)binaryWriter.BaseStream.Position + 0x4000;
+					var program = ProgramReader.Read(document);
 
-				compiler.Compile(program, address, binaryWriter);
+					program.Name = file.Path;
 
-				Console.WriteLine(binaryWriter.BaseStream.Position);
-
-				binaryWriter.BaseStream.SetLength(1024 * 16);
+					if (file.Address == null)
+						image.Add(program, file.Type == ProjectFileType.Class);
+					else if (file.Block == null)
+						image.Add(program, file.Address, file.Type == ProjectFileType.Class);
+					else
+						image.Add(program, file.Address, file.Block.Value, file.Type == ProjectFileType.Class);
+				}
+				catch (Exception e)
+				{
+					throw new Exception("Error Reading File: " + filename + "\r\n" + e.Message, e);
+				}
 			}
+
+			// Save Disk Image
+			Console.WriteLine("Writing C64Console.bin");
+
+			using (Stream stream = File.Create("C64Console.bin"))
+				image.Save(stream);
+
+			//var compiler = new BinaryCompiler();
+
+			//var program = ProgramBuilder.Build("../../C64Cartridge.xml");
+			////var program = ProgramBuilder.Build("../../C64Program.xml");
+
+			//var address = new MemoryAddress { Offset = 0x8000 };
+			////var address = new MemoryAddress { Offset = 0x07ff };
+
+			//using(Stream stream = File.Create("C64Console.bin"))
+			////using (Stream stream = File.Create("C64Console.prg"))
+			//using (BinaryWriter binaryWriter = new BinaryWriter(stream))
+			//{
+			//	compiler.Compile(program, address, binaryWriter);
+
+			//	program = ProgramBuilder.Build(
+			//		"../../C64Console.xml",
+			//		new KeyValuePair<string, string>[]
+			//		{
+			//			new KeyValuePair<string, string>("http://metalx.org/C64Console/Functions/Screen", "../../Functions/Screen.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/C64Console/Functions/System", "../../Functions/System.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/6502/Functions/Class", "../../../OZone/Functions/6502/Class.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/6502/Functions/String", "../../../OZone/Functions/6502/String.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/6502/Functions/Memory", "../../../OZone/Functions/6502/Memory.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/6502/Functions/MemoryManager", "../../../OZone/Functions/6502/MemoryManager.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Commodore64/Kernel", "../../../OZone/Platforms/Commodore/C64/Kernel.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Commodore64/Keyboard", "../../../OZone/Platforms/Commodore/C64/Keyboard.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Commodore64/System", "../../../OZone/Platforms/Commodore/C64/System.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Commodore64/Video", "../../../OZone/Platforms/Commodore/C64/Video.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Commodore64/Petscii", "../../../OZone/Platforms/Commodore/C64/Petscii.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Commodore64/Color", "../../../OZone/Platforms/Commodore/C64/Color.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Ansi/Ascii", "../../../OZone/Platforms/Ansi/Ascii/Ascii.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Variable", "../../../OZone/Structures/6502/Variable.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Class", "../../../OZone/Structures/6502/Class.xslt"),
+			//			new KeyValuePair<string, string>("http://metalx.org/Mos/6502/Operators", "../../../OZone/Platforms/Mos/6502/Operators.xslt")
+			//		});
+
+			//	//address.Offset = 0x8000;
+			//	//address.Offset = 0x810;
+			//	address.Offset = (uint)binaryWriter.BaseStream.Position + 0x8000;
+			//	//address.Offset = (uint)binaryWriter.BaseStream.Position + 0x4000;
+
+			//	compiler.Compile(program, address, binaryWriter);
+
+			//	Console.WriteLine(binaryWriter.BaseStream.Position);
+
+			//	binaryWriter.BaseStream.SetLength(1024 * 16);
+			//}
 		}
 	}
 }
---
+++ C64Console/Images/ProgramImage.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OZone.Programs;
+using System.IO;
+using OZone.Programs.Compilers;
+
+namespace C64Console
+{
+	public class ProgramImage
+	{
+		private List<ProgramImageProgram> _programs = new List<ProgramImageProgram>();
+		private int _block = 3;
+		private MemoryAddress _address = new MemoryAddress { Offset = 0x100000 };
+		private const int _startupAddress = 0x100000;
+		private const int _catalogAddress = 0x5000;
+		private const int _blockLength = 512;
+		private const int _blockCount = 2880;
+		private const int _diskLength = _blockLength * _blockCount;
+		private static readonly int[] _diskInfoBlocks = new int[] { 1, 2 };
+		private const int _bootLoaderCount = 8;
+
+		public void Add(Program program, bool catalog)
+		{
+			int length = (int)program.Segments.Sum(p => p.GetLength());
+			int blocks = (int)Math.Ceiling((decimal)length / (decimal)_blockLength);
+
+			_programs.Add(
+				new ProgramImageProgram
+				{
+					Block = _block,
+					Length = blocks,
+					Address = new MemoryAddress { Segment = _address.Segment, Offset = _address.Offset },
+					Program = program,
+					Catalog = catalog
+				});
+
+			_block += blocks;
+			_address.Offset += (uint)blocks * (uint)_blockLength;
+		}
+
+		public void Add(Program program, MemoryAddress address, bool catalog)
+		{
+			int length = (int)program.Segments.Sum(p => p.GetLength());
+			int blocks = length / _blockLength;
+
+			if (length % _blockLength != 0)
+				blocks++;
+
+			_programs.Add(
+				new ProgramImageProgram
+				{
+					Block = _block,
+					Length = blocks,
+					Address = new MemoryAddress
+					{
+						Offset = address.Offset,
+						Segment = address.Segment
+					},
+					Program = program,
+					Catalog = catalog
+				});
+
+			_block += blocks;
+		}
+
+		public void Add(Program program, MemoryAddress address, int block, bool catalog)
+		{
+			int length = (int)program.Segments.Sum(p => p.GetLength());
+			int blocks = length / _blockLength;
+
+			if (length % _blockLength != 0)
+				blocks++;
+
+			_programs.Add(
+				new ProgramImageProgram
+				{
+					Block = block,
+					Length = blocks,
+					Address = new MemoryAddress
+					{
+						Offset = address.Offset,
+						Segment = address.Segment
+					},
+					Program = program,
+					Catalog = catalog
+				});
+		}
+
+		public void Save(Stream stream)
+		{
+			var compiler = new BinaryCompiler();
+
+			using (var memory = new MemoryStream((int)_diskLength))
+			using (var writer = new BinaryWriter(memory))
+			{
+				// Write Programs
+				foreach (var program in _programs)
+				{
+					memory.Position = program.Block * _blockLength;
+					compiler.Compile(program.Program, program.Address, writer);
+				}
+
+				// Write Program List
+				int programs = _block;
+				int programsLength = (int)Math.Ceiling((((decimal)_programs.Count * 12.0m) + 4.0m) / (decimal)_blockLength);
+
+				_block += programsLength;
+
+				int catalog = _block;
+				int catalogLength = (int)Math.Ceiling(((decimal)(_programs.Count(p => p.Catalog)) * 4.0m) / (decimal)_blockLength);
+
+				_block += catalogLength;
+
+				int objects = _block++;
+				int indexes = _block++;
+				int name = _block++;
+
+				memory.Position = programs * _blockLength;
+
+				writer.Write((uint)(_programs.Count));
+
+				foreach (var program in _programs.Skip(1))
+				{
+					writer.Write((uint)program.Block);
+					writer.Write((uint)program.Length);
+					writer.Write((uint)program.Address.Offset);
+				}
+
+				writer.Write((uint)catalog);
+				writer.Write((uint)catalogLength);
+				writer.Write((uint)_catalogAddress);
+
+				// Write Catalog
+				memory.Position = catalog * _blockLength;
+
+				writer.Write((uint)(_programs.Count(p => p.Catalog)));
+
+				foreach (var program in _programs.Where(p => p.Catalog).OrderBy(p => Path.GetFileName(p.Program.Name)))
+					writer.Write((uint)program.Address.Offset);
+
+				// Write Object List
+				memory.Position = objects * _blockLength;
+
+				writer.Write((uint)0);
+
+				// Write Index List
+				memory.Position = indexes * _blockLength;
+
+				writer.Write((uint)0);
+
+				// Write Disk Name
+				memory.Position = name * _blockLength;
+
+				var diskName = new StringValue("OZone Operating System v1.0");
+				writer.Write(diskName.GetLength());
+				diskName.Write(writer);
+
+				// Write Disk Info Blocks
+				foreach (int block in _diskInfoBlocks)
+				{
+					memory.Position = block * _blockLength;
+
+					// Signature
+					writer.Write("HD14".ToCharArray());
+
+					// Version
+					writer.Write((uint)0);
+
+					// Name
+					writer.Write((uint)name);
+					writer.Write((uint)1);
+
+					// Programs
+					writer.Write((uint)programs);
+					writer.Write((uint)programsLength);
+
+					// Catalog
+					writer.Write((uint)catalog);
+					writer.Write((uint)catalogLength);
+
+					// Objects
+					writer.Write((uint)objects);
+					writer.Write((uint)1);
+
+					// Indexes
+					writer.Write((uint)indexes);
+					writer.Write((uint)1);
+
+					// Next Block
+					writer.Write((uint)_block);
+
+					writer.Write((uint)_startupAddress);
+				}
+
+				memory.SetLength(_diskLength);
+
+				byte[] data = memory.GetBuffer();
+
+				data[510] = 0x55;
+				data[511] = 0xaa;
+
+				stream.Write(data, 0, data.Length);
+			}
+		}
+
+		private struct ProgramImageProgram
+		{
+			public int Block;
+			public int Length;
+			public MemoryAddress Address;
+			public Program Program;
+			public bool Catalog;
+		}
+	}
+}
